6c172d6e9711b542235e9aafdf3fc8334c4f2956
ingenierodrigo <ingenierodrigo.net@gmail.com>
2021-11-30 20:25:34 -0500

    Prueba 2

---
+++ ejercicios-condicionales-complejas/ejercicio1.php
@@ -0,0 +1,18 @@
+<?php
+
+	$edad = 16;
+    
+    if ($edad>0 && $edad<=5) {
+        echo " Infante";
+    } 
+    if ($edad>5 && $edad<=10) {
+        echo " Niño";
+    }
+
+    if ($edad>10 && $edad<=15) {
+        echo " Pre Adolescente";
+    }  
+    if ($edad>15 && $edad<=18) {
+        echo " Adolescente";
+    }
+?>
